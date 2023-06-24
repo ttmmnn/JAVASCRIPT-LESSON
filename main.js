be577c567c1a371db40a6f3088cd0d4aed871e67
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-24 21:31:44 +0900

    update_practice

---
+++ main.js
@@ -0,0 +1,23 @@
+// console.log(add(3, 4));
+function add(a, b) {
+  return a + b;
+}
+add(3, 4);
+// console.dir(add);
+// console.log(add.name);
+// console.log(add.length);
+const newAdd = add;
+// console.log(newAdd(3, 4));
+let sayhi = function hi() {};
+sayhi = function () {
+  return 'hi';
+};
+// console.log(sayhi());
+
+const person = {
+  name: 'John',
+  sayHi: function () {
+    return 'hi';
+  },
+};
+console.log(person.sayHi());
